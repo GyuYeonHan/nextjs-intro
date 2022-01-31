bba75bdf0ba9a52d944289bd9707ccf0f10debc9
gyuyeon <gks951020@gmail.com>
2022-01-31 21:38:02 +0900

    my first nextJS

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import NavBar from "../Components/NavBar";
+import Head from "next/head";
+import Seo from "../Components/Seo";
+
+export default function about() {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1>About</h1>
+    </div>
+  );
+}
